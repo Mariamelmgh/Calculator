e64d76610166967608d4497d9719441b4c7e9362
Mariam EL MGHARAZ <melmghar@emeal.nttdata.com>
2023-08-08 21:35:54 +0100

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Mariam El Mgharaz on 8/8/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
